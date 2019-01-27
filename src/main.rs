287e71995652bcecb690a66becef5d16ce09bed5
tuckerthomas <tuckerthomas@github.com>
2019-01-27 18:47:01 -0500

    Working with stdout

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate nix;
 
-use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
 use std::{thread, time};
 
 fn main() {
-
     let handler = thread::spawn(|| {
-        let mut openvpn = Command::new("openvpn")
+        let mut openvpn = Command::new("sudo")
+            .arg("openvpn")
             .arg("--config")
             .arg("pia/CAToronto.ovpn")
             .stdout(Stdio::piped())
@@ -15,37 +15,39 @@ fn main() {
             .spawn()
             .expect("Could not spawn command 'openvpn'");
 
-        
         if let Some(ref mut stdout) = openvpn.stdout {
             let reader = BufReader::new(stdout);
-    
-            reader
-                .lines()
-                .filter_map(|line| line.ok())
-                .for_each(|line| println!("[OpenVPN] {}", line));
-        }
 
-        let addrs = nix::ifaddrs::getifaddrs().unwrap();
+            let mut cur_line = String::new();
 
-        for ifaddr in addrs {
-            match ifaddr.address {
-                Some(address) => {
-                    println!("interface {} address {}", ifaddr.interface_name, address);
-                },
+            let mut lines = reader.lines();
 
-                None => {
-                    println!("interface {} with usupported address family",
-                        ifaddr.interface_name);
-                }
+            for line in &mut lines {
+                println!("[OpenVPN]: {}", line);
             }
         }
-        
 
         let status = openvpn.wait();
         println!("Status: {:?}", status);
-
     });
-    
+
+    let addrs = nix::ifaddrs::getifaddrs().unwrap();
+
+    for ifaddr in addrs {
+        match ifaddr.address {
+            Some(address) => {
+                println!("interface {} address {}", ifaddr.interface_name, address);
+            }
+
+            None => {
+                println!(
+                    "interface {} with usupported address family",
+                    ifaddr.interface_name
+                );
+            }
+        }
+    }
+
     handler.join().expect("Thread Panicked");
 
     println!("Hello, world!");
